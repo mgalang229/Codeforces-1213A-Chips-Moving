ec51cad269bc958e329c59d62c2aebd94487d4cd
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-18 11:13:12 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	int even = 0;
+	int odd = 0;
+	// find the minimum between the parity of the numbers because
+	// if all numbers have the same parity then all moves are free
+	// however, for every different parity, we need to count it and it
+	// will represent as the minimum number of coins needed to move all 
+	// of these chips
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		if (x % 2 == 0) {
+			even++;
+		} else {
+			odd++;
+		}
+	}
+	cout << min(even, odd) << '\n';
+	return 0;
+}
